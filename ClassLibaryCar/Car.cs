e17d134a6668d430fa06a4f21a96656672c90bfc
Rasmus Avndal Hansen <rah001@edu.zealand.dk>
2024-10-04 11:37:19 +0200

    Add project files.

---
+++ ClassLibaryCar/Car.cs
@@ -0,0 +1,40 @@
+﻿public class Car
+{
+
+    public int Id { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+
+    public override string ToString()
+    {
+        return $"{Id} {Model} {Year}";
+    }
+
+    public void ValidateModel()
+    {
+        if (Model == null)
+        {
+            throw new ArgumentNullException("Model was null");
+        }
+        if (Model.Length < 1)
+        {
+            throw new ArgumentOutOfRangeException("Title must be at least 1 character");
+        }
+    }
+
+    public void ValidateYear()
+    {
+        if (Year == null)
+        {
+            throw new ArgumentNullException("Year was null");
+        }
+    }
+
+    public void Validate()
+    {
+        ValidateModel();
+        ValidateYear();
+    }
+
+}
---
+++ ClassLibaryCar/CarRepositoryList.cs
@@ -0,0 +1,106 @@
+﻿public class CarRepositoryList
+{
+    private int _nextId = 1;
+    private readonly List<Car> _cars = new();
+
+    public CarRepositoryList()
+    {
+        _cars.Add(new Car() { Id = _nextId++, Model = "BMW", Year = 2020 });
+        _cars.Add(new Car() { Id = _nextId++, Model = "Audi", Year = 2019 });
+    }
+
+    public List<Car> GetAll()
+    {
+        return new List<Car>(_cars);
+    }
+
+
+    public Car GetById(int id)
+    {
+        return _cars.Find(c => c.Id == id);
+    }
+
+    public Car Add(Car car)
+    {
+        car.Validate();
+        car.Id = _nextId++;
+        _cars.Add(car);
+        return car;
+
+    }
+    public Car Remove(int id)
+    {
+        Car? car = GetById(id);
+
+        if (car.Id == null)
+        {
+            return null;
+        }
+
+        _cars.Remove(car);
+        return car;
+    }
+
+    public Car Update(Car car, int id)
+    {
+        car.Validate();
+        Car existingCar = GetById(id);
+
+        if (existingCar == null)
+        {
+            return null;
+        }
+
+        existingCar.Model = car.Model;
+        existingCar.Year = car.Year;
+
+        return car;
+
+    }
+
+    public IEnumerable<Car> Get(int? yearAfter = null, string? modelIncludes = null, string? orderBy = null)
+    {
+        IEnumerable<Car> result = new List<Car>(_cars);
+
+        // Filtering
+
+        if (yearAfter != null)
+        {
+            result = result.Where(car => car.Year > yearAfter);
+        }
+        if (modelIncludes != null)
+        {
+            result = result.Where(car => car.Model.Contains(modelIncludes));
+        }
+
+        // Ordering aka. sorting
+
+        if (orderBy != null)
+        {
+            orderBy = orderBy.ToLower();
+            switch (orderBy)
+            {
+                case "title": // fall through to next case
+                case "title_asc":
+                    result = result.OrderBy(m => m.Model);
+                    break;
+                case "title_desc":
+                    result = result.OrderByDescending(m => m.Model);
+                    break;
+                case "year":
+                case "year_asc":
+                    result = result.OrderBy(m => m.Year);
+                    break;
+                case "year_desc":
+                    result = result.OrderByDescending(m => m.Year);
+                    break;
+                default:
+                    break; // do nothing
+                           //throw new ArgumentException("Unknown sort order: " + orderBy);
+            }
+        }
+        return result;
+    }
+}
+
+
